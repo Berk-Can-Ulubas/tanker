7b7f11a8b11998b92d1370a0c70f4f080eb79c50
Berk-Can-Ulubas <berkcan.ulubas@gmx.de>
2024-10-31 12:20:13 +0100

    _

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -4,15 +4,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import space.earlygrey.shapedrawer.ShapeDrawer;
 
 import java.awt.*;
 
 import static com.mygdx.game.TankerGdxGame.INSTANCE;
 
 public class MainMenuScreen extends ScreenAdapter {
-
-    public static final int BUTTON_SIZE = 100;
 
     private Texture playButtonActive;
     private Texture playButtonInactive;
@@ -23,7 +23,10 @@ public class MainMenuScreen extends ScreenAdapter {
     private Texture exitButtonActive;
     private Texture exitButtonInactive;
 
+   private ShapeRenderer shape = new ShapeRenderer();
+
     public MainMenuScreen() {
+
     }
 
     @Override
@@ -43,28 +46,43 @@ public class MainMenuScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        INSTANCE.WINDOW_WIDTH = Gdx.graphics.getWidth();
-        INSTANCE.WINDOW_HEIGHT = Gdx.graphics.getHeight();
+        // Use the updated dimensions
+        int windowWidth = INSTANCE.WINDOW_WIDTH;
+        int windowHeight = INSTANCE.WINDOW_HEIGHT;
+        int buttonSize = windowHeight / 12;
 
         ScreenUtils.clear(0.169f, 0.749f, 0.416f, 1f);
+
+        // Draw a rectangle
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.setColor(0, 0, 1, 1f);
+        int rectWidth = windowWidth / 4;
+        int rectHeight = windowHeight / 2;
+        shape.rect(windowWidth / 2 - rectWidth / 2, windowHeight / 2 - rectHeight / 2, rectWidth, rectHeight);
+        shape.end();
+
         INSTANCE.batch.begin();
 
         // Draw the buttons
-        int x = INSTANCE.WINDOW_WIDTH / 2 - BUTTON_SIZE;
-        INSTANCE.batch.draw(playButtonInactive, x, 575, BUTTON_SIZE, BUTTON_SIZE);
-        INSTANCE.batch.draw(settingsButtonInactive, x, 450, BUTTON_SIZE, BUTTON_SIZE);
-        INSTANCE.batch.draw(achievementsButtonInactive, x, 325, BUTTON_SIZE, BUTTON_SIZE);
-        INSTANCE.batch.draw(exitButtonInactive, x, 200, BUTTON_SIZE, BUTTON_SIZE);
+        int x = windowWidth / 2 - buttonSize / 2;
+        int y1 = (1 * windowHeight / 4) - (3 * buttonSize / 2);
+        int y2 = (1 * windowHeight / 2) - (3 * buttonSize / 2);
+        int y3 = (3 * windowHeight / 4) - (3 * buttonSize / 2);
+        int y4 = (4 * windowHeight / 4) - (3 * buttonSize / 2);
 
-        if (Gdx.input.getX() < x + BUTTON_SIZE && Gdx.input.getX() > x && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() < 575 + BUTTON_SIZE && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() > 575) {
-            INSTANCE.batch.draw(playButtonActive, x, 575, BUTTON_SIZE, BUTTON_SIZE);
-        } else if(Gdx.input.getX() < x + BUTTON_SIZE && Gdx.input.getX() > x && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() < 450 + BUTTON_SIZE && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() > 450) {
-            INSTANCE.batch.draw(settingsButtonActive, x, 450, BUTTON_SIZE, BUTTON_SIZE);
-        } else if(Gdx.input.getX() < x + BUTTON_SIZE && Gdx.input.getX() > x && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() < 325 + BUTTON_SIZE && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() > 325) {
-            INSTANCE.batch.draw(achievementsButtonActive, x, 325, BUTTON_SIZE, BUTTON_SIZE);
-        } else if(Gdx.input.getX() < x + BUTTON_SIZE && Gdx.input.getX() > x && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() < 200 + BUTTON_SIZE && INSTANCE.WINDOW_HEIGHT - Gdx.input.getY() > 200) {
-            INSTANCE.batch.draw(exitButtonActive, x, 200, BUTTON_SIZE, BUTTON_SIZE);
+        INSTANCE.batch.draw(playButtonInactive, x, y4, buttonSize, buttonSize);
+        INSTANCE.batch.draw(settingsButtonInactive, x, y3, buttonSize, buttonSize);
+        INSTANCE.batch.draw(achievementsButtonInactive, x, y2, buttonSize, buttonSize);
+        INSTANCE.batch.draw(exitButtonInactive, x, y1, buttonSize, buttonSize);
 
+        if (Gdx.input.getX() < x + buttonSize && Gdx.input.getX() > x && windowHeight - Gdx.input.getY() < y4 + buttonSize && windowHeight - Gdx.input.getY() > y4) {
+            INSTANCE.batch.draw(playButtonActive, x, y4, buttonSize, buttonSize);
+        } else if (Gdx.input.getX() < x + buttonSize && Gdx.input.getX() > x && windowHeight - Gdx.input.getY() < y3 + buttonSize && windowHeight - Gdx.input.getY() > y3) {
+            INSTANCE.batch.draw(settingsButtonActive, x, y3, buttonSize, buttonSize);
+        } else if (Gdx.input.getX() < x + buttonSize && Gdx.input.getX() > x && windowHeight - Gdx.input.getY() < y2 + buttonSize && windowHeight - Gdx.input.getY() > y2) {
+            INSTANCE.batch.draw(achievementsButtonActive, x, y2, buttonSize, buttonSize);
+        } else if (Gdx.input.getX() < x + buttonSize && Gdx.input.getX() > x && windowHeight - Gdx.input.getY() < y1 + buttonSize && windowHeight - Gdx.input.getY() > y1) {
+            INSTANCE.batch.draw(exitButtonActive, x, y1, buttonSize, buttonSize);
         }
 
         INSTANCE.batch.end();
@@ -77,6 +95,12 @@ public class MainMenuScreen extends ScreenAdapter {
         achievementsButtonInactive.dispose();
         settingsButtonInactive.dispose();
         exitButtonInactive.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        INSTANCE.WINDOW_WIDTH = width;
+        INSTANCE.WINDOW_HEIGHT = height;
     }
 
     @Override
